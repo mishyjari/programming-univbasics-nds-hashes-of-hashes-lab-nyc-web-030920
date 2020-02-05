796421dda8ad9b3ed01efef09e28aca5831ac6a2
Michelle Frattaroli <mishyjari@outlook.com>
2020-02-05 16:38:16 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -43,12 +43,18 @@ def naming_system
   # HoH all the way to Species!
   {
     :Kingdom => {
-      :Phylum => {
-        :Class => {
-          :Order => {
-            :Family => {
-              :Genus => {
-                :Species => nil
+      :label => 'Phylum',
+      :sub_category => {
+        :label => 'Class',
+        :sub_category => {
+          :label => 'Order',
+          :sub_category => {
+            :label => 'Family',
+            :sub_category => {
+              :label: 'Genus',
+              :sub_category => {
+                :label => 'Species'
+                :sub_category => nil
               }
             }
           }
